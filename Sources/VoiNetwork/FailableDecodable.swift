519bc3086bae89a7e6f9c9218d127d2dad6f91e5
Mayur Deshmukh <mayur.deshmukh@voiapp.io>
2020-10-21 20:23:51 +0200

    Make FailableDecodable public

--- Sources/VoiNetwork/FailableDecodable.swift
+++ Sources/VoiNetwork/FailableDecodable.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 /// `FailableDecodable` helps us not fail when we have an array of Decodable elements. When we try to decode a JSON data containing one or more corrupt JSON objects in an array, we do not want to fail the entire main decodable. Hence we should use this helper class wherever we have arrays in Decodables, and filter out the failed objects using `compactMap()`
-struct FailableDecodable<Base: Decodable>: Decodable {
+public struct FailableDecodable<Base: Decodable>: Decodable {
 
     let base: Base?
 
-    init(from decoder: Decoder) throws {
+    public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         self.base = try? container.decode(Base.self)
     }
